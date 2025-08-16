a1afcfc612e6f760d51e2e8636bb3ffd5813d983
nhidinh2 <nhidinh2@illinois.edu>
2025-08-16 09:14:35 -0500

    y

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Blog from './components/pages/Blog';
+import IctServicePage from './components/pages/IctServicePage';
+import MeServicePage from './components/pages/MeServicePage';
+import BusinessApplicationServicePage from './components/pages/BusinessApplicationServicePage';
+import BroadcastingServicePage from './components/pages/BroadcastingServicePage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl font-bold text-center py-20 text-blue-600">
-        Hello from NetCorp!
-      </h1>
-      <p className="text-center text-gray-600">
-        If you can see this, React is working!
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/services/ict" element={<IctServicePage />} />
+        <Route path="/services/me" element={<MeServicePage />} />
+        <Route path="/services/business-application" element={<BusinessApplicationServicePage />} />
+        <Route path="/services/broadcasting" element={<BroadcastingServicePage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

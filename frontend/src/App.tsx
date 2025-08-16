25b7b603815792c45f8a38db4b0ab90b418bfcad
nhidinh2 <nhidinh2@illinois.edu>
2025-08-16 09:18:28 -0500

    y

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,77 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AOS from 'aos';
+
+// Components
 import Navbar from './components/layout/Navbar';
+import About from './components/ui/About';
+import Clients from './components/ui/Clients';
+import Partners from './components/ui/Partners';
 import Footer from './components/layout/Footer';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
 import Contact from './components/pages/Contact';
-import Blog from './components/pages/Blog';
+import Home from './components/pages/Home';
 import IctServicePage from './components/pages/IctServicePage';
-import MeServicePage from './components/pages/MeServicePage';
-import BusinessApplicationServicePage from './components/pages/BusinessApplicationServicePage';
 import BroadcastingServicePage from './components/pages/BroadcastingServicePage';
+import BusinessApplicationServicePage from './components/pages/BusinessApplicationServicePage';
+import MeServicePage from './components/pages/MeServicePage';
+import ScrollToTop from './components/ui/ScrollToTop';
+import Services from './components/ui/Services';
 
 function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    // Get the language from localStorage or default to 'en'
+    const savedLanguage = localStorage.getItem('language') || 'en';
+    i18n.changeLanguage(savedLanguage);
+  }, [i18n]);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/services/ict" element={<IctServicePage />} />
-        <Route path="/services/me" element={<MeServicePage />} />
-        <Route path="/services/business-application" element={<BusinessApplicationServicePage />} />
-        <Route path="/services/broadcasting" element={<BroadcastingServicePage />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="min-h-screen bg-light relative">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={
+            <div className="pt-20">
+              <About />
+            </div>
+          } />
+          <Route path="/about/home" element={<Navigate to="/" replace />} />
+          <Route path="/client" element={
+            <div className="pt-20">
+              <Clients />
+            </div>
+          } />
+          <Route path="/partners" element={
+            <div className="pt-20">
+              <Partners />
+            </div>
+          } />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={
+            <div className="pt-20">
+              <Services />
+            </div>
+          } />
+          <Route path="/services/ict" element={<IctServicePage />} />
+          <Route path="/services/broadcasting" element={<BroadcastingServicePage />} />
+          <Route path="/services/business-application" element={<BusinessApplicationServicePage />} />
+          <Route path="/services/me" element={<MeServicePage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
